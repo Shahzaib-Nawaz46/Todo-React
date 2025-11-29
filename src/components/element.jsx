d2df1c37ecf2dc28f299cf4086452fd9f5ed1ac1
Shahzaib-Nawaz46 <shahzaibkhan9k@gmail.com>
2025-11-28 23:30:22 -0800

    functionality added

--- src/components/element.jsx
+++ src/components/element.jsx
@@ -8,13 +8,76 @@ const element = props => {
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+  const [isEditing ,setediting] = useState(false);
+  const [editindex,setindex] = useState(null)
+
   function handleChange(event) {
     setValue(event.target.value);
     console.log(value);
   }
 
   function handleSubmit(event) {
-    setTodos([value]);
+      if (!value.trim()) return;
+      
+      if(isEditing){
+        const updated = [...todos];
+        updated[editindex] = value
+
+        setTodos(updated)
+        localStorage.getItem("todo",JSON.stringify(updated))
+        setediting(false);
+        setindex(null);
+        setValue("");
+
+      } else {
+
+        setTodos([...todos,value]);
+        setValue("");
+      }
+  }
+
+  function startEditing(index) {
+  setValue(todos[index]);   // put the todo into input field
+  setediting(true);
+  setindex(index);
+}
+
+  
+  // saving todos to local storage
+  useEffect(()=>{
+     if(todos.length ===0) return ;
+    const saved = localStorage.setItem("todos",JSON.stringify(todos));
+    if(saved){    
+      console.log(todos,"by useeefect");
+
+    }
+
+  },[todos]);
+
+  // getting todos from local storage
+  useEffect(()=>{
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if(savedTodos){
+     console.log(savedTodos,"getting from local storage");
+      setTodos(savedTodos);
+      
+    }
+  },[]);
+
+  //removing item from local storage and ui
+  function removetodo(todo){
+    const removed = todos.filter((item)=> item !== todo);
+    localStorage.setItem("todos",JSON.stringify(removed));
+    setTodos(removed);
+    console.log(todos,"removed item");
+  }
+
+  //editing item 
+  function editing(item,index){
+    const edit = localStorage.getItem("todos")
+    edit[index] = item
+    
+
   }
   return (
     <div className='flex flex-col align-center items-center  p-7  w-full font-sans  '>
@@ -34,23 +97,16 @@ const element = props => {
         <h2 className='flex p-2 m-2 font-semibold text-lg'>Your Todos</h2>
 
 
-        <div className='flex m-2 p-1 gap-2 w-full'>
-          <input type="checkbox" name="" id="" />
-          <p className='flex'>Sample Todo 1</p>
+        
 
-
-          <FaEdit className='ml-auto' size={22} color="blue" />
-          <FaTrash className='' size={22} color="red" />
-        </div>
-
-           {todos.map((todo) =>(
-            <div className='flex m-2 p-1 gap-2 w-full'>
+           {todos.map((todo,index) =>(
+            <div key = {index} className='flex m-2 p-1 gap-2 w-full'>
           <input type="checkbox" name="" id="" />
           <p className='flex'>{todo}</p>
 
 
-          <FaEdit className='ml-auto' size={22} color="blue" />
-          <FaTrash className='' size={22} color="red" />
+          <FaEdit className='ml-auto' size={22} color="blue"   onClick={() => startEditing(index)}/>
+          <FaTrash className='' size={22} color="red" onClick={()=> removetodo(todo)} />
         </div>
            ))}
 
